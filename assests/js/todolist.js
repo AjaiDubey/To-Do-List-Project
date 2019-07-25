f9a0458ff4539a1df6ee1b71f51e15cd69e03941
AjaiDubey <ajaidubey22@gmail.com>
2019-07-26 00:49:08 +0530

    Done a little change in the input and lis.

--- assests/js/todolist.js
+++ assests/js/todolist.js
@@ -1,10 +1,21 @@
-$("li").click(function(){
+$("ul").on("click", "li",function(){
 	$(this).toggleClass("completed");
 });
 
-$("span").click(function(event){
+$("ul").on("click", "span", function(event){
 	$(this).parent().fadeOut(500, function(){
 		$(this).remove();
 	});
 	event.stopPropagation();
+})
+
+$("input[type = 'text']").keypress(function(ev){
+
+	
+	if(ev.which === 13){
+
+		var ntodo = $(this).val();
+		$(this).val("");
+		$("ul").append("<li><span>x</span> " + ntodo + " </li>")
+	}
 })
